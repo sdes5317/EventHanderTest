0e6e6e2e7e7dea7a3c15839c558bc92bc6ead320
HaoChih <sdes5317@yahoo.com.tw>
2019-02-15 11:53:46 +0800

    注解

--- MelsecHandler/Handler.cs
+++ MelsecHandler/Handler.cs
@@ -16,11 +16,11 @@ namespace MelsecHandler
             {
                 throw new LengthException();
             }
-            var classmember = typeof(Handler).GetNestedTypes();//列出全部的子類
+            var classmember = typeof(Handler).GetNestedTypes();//列出全部的public子類
             foreach (var item in classmember)
             {
                 //Console.WriteLine(item);
-                Type tp = Type.GetType(item.ToString());//取得子類的名子 只有public的
+                Type tp = Type.GetType(item.ToString());//取得子類的名子
                 object NewObject = Activator.CreateInstance(tp, true);//實例一個子類 裝箱的狀態
                 Melsec MelsecObj = (Melsec)NewObject;//拆箱為Melsec類
                 //Console.WriteLine("類型:{0} 表頭:{1}", MelsecObj.GetType(), MelsecObj.header);
